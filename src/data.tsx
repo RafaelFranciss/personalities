c551ef66a45a0c01c8561c25abb3698ed4249c22
Francis Acuna <158844285+RafaelFranciss@users.noreply.github.com>
2025-02-17 01:22:12 +0800

    Update data.tsx

--- src/data.tsx
+++ src/data.tsx
@@ -2,51 +2,51 @@ export const singerList = [{
     name: 'NIKI',
     description: 'Made a name for herself in the R&B and pop scenes with her ethereal voice and introspective songwriting. She gained international recognition with hits like "lowkey" and "ngl," blending soft melodies with deep, relatable lyrics. She’s also part of the 88rising collective, bringing an Asian perspective to the global music stage.',
     url: 'https://100comments.com/wp-content/uploads/2022/08/Photo-1_-Artwork-Album-Nicole.jpg',
-    alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
+    alt: 'A picture of NIKI.'
   }, {
     name: 'Billie Eilish',
     description: 'With her unique blend of alternative pop, electropop, and dark themes, Billie Eilish quickly rose to fame. Known for her haunting voice and bold, genre-defying style, she became the youngest artist to win the "Big Four" Grammy Awards in one night at just 18 years old.',
     url: 'https://www.beverlyhillsmagazine.com/wp-content/uploads/Beverly-Hills-Magazine-Celebrity-Billie-Eilish-shutterstock_1638674032.jpg',
-    alt: 'A gigantic metallic flower sculpture with reflective mirror-like petals and strong stamens.'
+    alt: 'A picture of Billie Eilish.'
   }, {
     name: 'SZA',
     description: 'An American singer-songwriter, SZA is a prominent figure in the R&B scene, known for her ethereal vocals and deeply emotional lyrics. Her album Ctrl was a breakout success, earning critical acclaim and massive popularity with tracks like "Love Galore" and "The Weekend." She blends R&B with soul, neo-soul, and elements of alternative music.',
     url: 'https://media.pitchfork.com/photos/638902d5f777c8e284615da3/2:1/w_2560%2Cc_limit/SZA.jpg',
-    alt: 'The sculpture depicting a human head seems ever-present and solemn. It radiates calm and serenity.'
+    alt: 'A picture of SZA.'
   }, {
     name: 'Post Malone',
     description: 'An American singer, songwriter, and record producer. He rose to fame with his debut single, "White Iverson," in 2015, which garnered significant attention and marked the start of his successful career. Known for his unique blend of hip-hop, pop, and rock influences, Post Malone sound is characterized by catchy melodies, laid-back vocals, and introspective lyrics.',
     url: 'https://wp.hnhh.com/wp-content/uploads/cover/12/97/1619119712_09784ce276c721c5c02f37fc92f9c587.jpg',
-    alt: 'Three monumental stone busts with the heads that are disproportionately large with somber faces.'
+    alt: 'A picture of Post Malone.'
   }, {
     name: 'Sam Smith',
     description: 'A British singer-songwriter known for their soulful voice and emotional ballads, Sam Smith rose to fame with songs like "Stay With Me" and "Im Not the Only One." Their music blends pop and soul, often dealing with themes of love and heartbreak.',
     url: 'https://www.rollingstone.com/wp-content/uploads/2023/01/Sam-Smith.jpg?w=1581&h=1054&crop=1',
-    alt: 'A large mosaic sculpture of a whimsical dancing female figure in a colorful costume emanating joy.'
+    alt: 'A picture of Sam Smith.'
   }, {
     name: 'Frank Ocean',
     description: 'An American singer, songwriter, and producer, Frank Ocean is widely regarded as one of the most influential contemporary artists. His music blends R&B, soul, and electronic influences, with albums like Channel Orange and Blonde featuring deeply personal lyrics and unique sounds that have earned critical acclaim.',
     url: 'https://www.popsugar.com.au/wp-content/uploads/sites/2/2023/03/0ef06b23641cea9c773852.74371827_-1.jpg',
-    alt: 'A tall sculpture made of three elements stacked on each other reminding of a human figure.'
+    alt: 'A picture of Frank Ocean.'
   }, {
     name: 'Sabrina Carpenter',
     description: "An American singer-songwriter and actress, Sabrina Carpenter rose to fame with her roles on Disney Channel shows like Girl Meets World. She transitioned into music with pop hits like Thumbs and Why, known for her catchy melodies and relatable lyrics. Her music explores themes of love, self-reflection, and growing up.",
     url: 'https://cdn.cliqueinc.com/posts/303884/sabrina-carpenter-ama-makeup-303884-1669050726452-main.700x0c.jpg',
-    alt: 'An intricate wood sculpture of a warrior with a focused face on a horse adorned with patterns.'
+    alt: 'A picture of Sabrina Carpenter.'
   }, {
     name: 'Daniel Caesar',
     description: "A Canadian R&B singer-songwriter, Daniel Caesar is known for his smooth voice and soulful, emotional music. With hits like Get You and Best Part (featuring H.E.R.), his music blends contemporary R&B with elements of gospel and soul, earning him critical acclaim and a devoted following.",
     url: 'https://fghsnews.com/wp-content/uploads/2019/12/danielcaesar-900x576.jpg',
-    alt: 'The sculpture reminds a cascade of folds, quite different from bellies in classical sculptures.'
+    alt: 'A picture of Daniel Caesar.'
   }, {
     name: 'Olivia Rodrigo',
     description: 'An American singer-songwriter and actress, Olivia Rodrigo became a global sensation with her debut album SOUR. With hits like "drivers license," "good 4 u," and "deja vu," she quickly became a pop icon, known for her emotionally raw lyrics, relatable themes of heartbreak, and alt-pop sound.',
     url: 'https://fashionista.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_700/MTkzNTgwNjY0NjUyNTcyMTQ0/olivia-rodrigo-red-carpet--3.jpg',
-    alt: '12 terracotta sculptures of solemn warriors, each with a unique facial expression and armor.'
+    alt: 'A picture of Olivia Rodrigo.'
   }, {
     name: 'Keshi',
     description: 'A Vietnamese - American singer, songwriter, and producer, Keshi is known for his unique blend of R&B, lo-fi pop, and alternative sounds. Gaining popularity with songs like "2 Soon," and "Right Here" his smooth voice, introspective lyrics, and emotive style have made him a standout in the modern music scene. His music often explores themes of love, heartbreak, and personal growth, and he has built a dedicated fanbase through his self-produced tracks.',
     url: 'https://images.squarespace-cdn.com/content/v1/574f3465746fb9ccc22d890f/927ef2e3-9070-4d4e-8684-a321a5b92053/KESHI+GABRIEL+COVER+.jpg',
-    alt: 'A black matte sculpture where the individual elements are initially indistinguishable.'
+    alt: 'A picture of Keshi.'
   }];
   
